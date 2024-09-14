c5a69de938a08b6ddda53353f25f46ba4d36f4f0
DPRAKA15 <dpraka15@ford.com>
2024-09-14 17:17:39 +0530

    feat: Home page

---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+import { LANDING_PAGE_MENU, MenuProps } from "./menu"
+
+type LearnByConstantsProps = {
+    landingPageMenu: MenuProps[]
+    // signUpForm: AuthFormProps[]
+    // signInForm: AuthFormProps[]
+    // groupList: GroupListProps[]
+    // createGroupPlaceholder: CreateGroupPlaceholderProps[]
+    // groupPageMenu: GroupMenuProps[]
+}
+
+export const LEARNBY_CONSTANTS: LearnByConstantsProps = {
+    landingPageMenu: LANDING_PAGE_MENU,
+    // signUpForm: SIGN_UP_FORM,
+    // signInForm: SIGN_IN_FORM,
+    // groupList: GROUP_LIST,
+    // createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
+    // groupPageMenu: GROUP_PAGE_MENU,
+}
